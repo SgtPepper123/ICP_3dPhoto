790c87dab81c13c5cff80a9d77269ebed5ac7034
Jan Rüegg <rggjan@gmail.com>
2011-03-30 15:00:11 +0200

    c Formatting

--- kinect_icp/src/icp_core.cpp
+++ kinect_icp/src/icp_core.cpp
@@ -9,12 +9,11 @@ IcpCore::IcpCore(ros::Publisher publisher) : publisher_(publisher)
 
 void IcpCore::registerCloud(const PCloud::ConstPtr& new_point_cloud)
 { 
-	ROS_INFO("Received");
-	if(m_Clouds.begin()!=m_Clouds.end())
-	{
-		//IcpLocal algortihm(m_Clouds.end()-1,new_point_cloud);
-		//algorithm.compute();
-		
-	} 
-	m_Clouds.push_back(new_point_cloud);//*/
+  ROS_INFO("Received");
+  if(m_Clouds.begin()!=m_Clouds.end())
+  {
+    //IcpLocal algortihm(m_Clouds.end()-1,new_point_cloud);
+    //algorithm.compute();
+  } 
+  m_Clouds.push_back(new_point_cloud);
 }
