93d0feab4ddfd0ebd9f8d2d5790d340fb6bda7ef
Jan Rüegg <rggjan@gmail.com>
2011-03-30 15:05:17 +0200

    c Reformatting

--- kinect_icp/src/icp_local.cpp
+++ kinect_icp/src/icp_local.cpp
@@ -7,21 +7,21 @@ IcpLocal::IcpLocal(const PCloud::ConstPtr& first, const PCloud::ConstPtr& second
 : m_First(first)
 , m_Second(second)
 {
-	
 }
 	
 
 void IcpLocal::Compute(/*SomeMatrixClass initialTransformation*/)
 {
-	float error = std::numeric_limits<float>::max();
-	float old_error;
-	do{
-		old_error = error;
-		Selection();
-		Matching();
-		Rejecting();
-		error = Minimization();
-	}while(abs(old_error-error) > ITERATION_THRESHOLD);
+  float error = std::numeric_limits<float>::max();
+  float old_error;
+  do
+  {
+    old_error = error;
+    Selection();
+    Matching();
+    Rejecting();
+    error = Minimization();
+  } while(abs(old_error-error) > ITERATION_THRESHOLD);
 }
 
 		
@@ -48,7 +48,5 @@ void IcpLocal::Rejecting()
 
 float IcpLocal::Minimization()
 {
-	return 0.f;
-	
+  return 0.f;
 }
-
