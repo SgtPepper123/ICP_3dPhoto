6e22cdce0a13b7a34d35f83acccba1041253f106
Jan Rüegg <rggjan@gmail.com>
2011-03-30 14:30:15 +0200

    ! Corrected indents

--- kinect_icp/src/point_cloud_collector.cpp
+++ kinect_icp/src/point_cloud_collector.cpp
@@ -2,25 +2,24 @@
 #include "kinect_icp/icp_core.h"
 #include <boost/bind.hpp>
 
-int main(int argc, char **argv)
-{
-	//intialize some ros stuff
-	ros::init(argc, argv, "point_cloud_collector");
+int main(int argc, char **argv) {
+  //intialize some ros stuff
+  ros::init(argc, argv, "point_cloud_collector");
 
-	//create ros node to be able to send/receive messages
-	ros::NodeHandle n;
+  //create ros node to be able to send/receive messages
+  ros::NodeHandle n;
 
-	//entrypoint of the Icp Algorithm
-	kinect_icp::IcpCore core;
+  //entrypoint of the Icp Algorithm
+  kinect_icp::IcpCore core;
 
-	//create function pointer to registerCloud function in IcpCore 
-	//then subscribe it with the rosout node, so all published pointclouds will be directed to the algorithm.
-	boost::function< void(const boost::shared_ptr< kinect_icp::PCloud const > &)> func = boost::bind(&kinect_icp::IcpCore::registerCloud,boost::ref(core),_1);
-	ros::Subscriber sub = n.subscribe("/camera/rgb/points", 1000, func);
+  //create function pointer to registerCloud function in IcpCore 
+  //then subscribe it with the rosout node, so all published pointclouds will be directed to the algorithm.
+  boost::function< void(const boost::shared_ptr< kinect_icp::PCloud const > &)> func = boost::bind(&kinect_icp::IcpCore::registerCloud,boost::ref(core),_1);
+  ros::Subscriber sub = n.subscribe("/camera/rgb/points", 1000, func);
 	
-	//wait for messages till canceled 
-	ros::spin();
+  //wait for messages till canceled 
+  ros::spin();
 
-	return 0;
+  return 0;
 }
 
