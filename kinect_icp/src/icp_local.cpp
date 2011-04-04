a1a52c68bee6ae4bb837f348a8e24ab0b4b9575f
Jan Rüegg <rggjan@gmail.com>
2011-04-04 09:33:48 +0200

    ! Endless loop fix + Added some INFOs

--- kinect_icp/src/icp_local.cpp
+++ kinect_icp/src/icp_local.cpp
@@ -17,14 +17,14 @@ void IcpLocal::Compute(/*SomeMatrixClass initialTransformation*/)
   ROS_INFO("IcpLocal::Compute");
   float error = std::numeric_limits<float>::max();
   float old_error;
-  do
+//  do
   {
     old_error = error;
     Selection();
     Matching();
     Rejecting();
     error = Minimization();
-  } while (abs(old_error-error) > ITERATION_THRESHOLD);
+  }// while (abs(old_error-error) > ITERATION_THRESHOLD);
 }
 
 		
@@ -38,6 +38,7 @@ void IcpLocal::Compute(/*SomeMatrixClass initialTransformation*/)
 		
 void IcpLocal::Selection()
 {
+  ROS_INFO("IcpLocal::Selection");
   srand(time(NULL));
   int i = 0;
   int count = first_->points.size();
@@ -51,12 +52,13 @@ void IcpLocal::Selection()
     if(x==x){    
       selected_.push_back(mp);      
     }
+    i++;
   }
-	
 }
 
 void IcpLocal::Matching()
 {
+  ROS_INFO("IcpLocal::Matching");
   int imax = selected_.size();
   for (int i=0; i<imax; i++)
   {
@@ -80,6 +82,7 @@ void IcpLocal::Matching()
 
 void IcpLocal::Rejecting()
 {
+  ROS_INFO("IcpLocal::Rejecting");
   const double threshold = 10;
 
   int threshold_squared = threshold*threshold;
